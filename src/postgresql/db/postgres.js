9a5e9e4f2171d443e620e1f6318dd6f61ec04215
Jongho Kim <april9288@gmail.com>
2019-04-22 06:56:04 -0700

    encrypt all the api keys

--- src/postgresql/db/postgres.js
+++ src/postgresql/db/postgres.js
@@ -1,22 +1,24 @@
 const knex = require('knex')
 
+let connection
+
+if (process.env.PORT === '3001') {
+	connection = {
+	    host : process.env.POSTGRES_LOCAL_HOST,
+	    user : process.env.POSTGRES_LOCAL_USER,
+	    password : process.env.POSTGRES_LOCAL_PASSWORD,
+	    database : process.env.POSTGRES_LOCAL_DATABASE
+	}
+} else {
+	connection = {
+		connectionString : process.env.DATABASE_URL,
+   		ssl: true
+	}
+}
+
 const postgres = knex({
   client: 'pg',
-  connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true
-  }
+  connection
 })
 
-//local env
-// const postgres = knex({
-//   client: 'pg',
-//   connection: {
-//     host : '127.0.0.1',
-//     user : 'postgres',
-//     password : '',
-//     database : 'postgres'
-//   }
-// })
-
-  module.exports = postgres
+module.exports = postgres
