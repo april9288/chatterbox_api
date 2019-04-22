8088b35fdb321cbb429ad3bc8bf0bfbf5a638e51
Jongho Kim <april9288@gmail.com>
2019-04-22 00:27:49 -0700

    added heroku postgrest addon

--- src/postgresql/db/postgres.js
+++ src/postgresql/db/postgres.js
@@ -1,12 +1,22 @@
 const knex = require('knex')
+
 const postgres = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '',
-    database : 'postgres'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true
   }
 })
+
+//local env
+// const postgres = knex({
+//   client: 'pg',
+//   connection: {
+//     host : '127.0.0.1',
+//     user : 'postgres',
+//     password : '',
+//     database : 'postgres'
+//   }
+// })
 
   module.exports = postgres
